e1f361ed603387d1d9b4c2d28ae3399a70973475
chuchumtech <shlomo@chuchumwebdesign.com>
2025-04-16 11:50:42 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
-const { Client, Environment } = require("@square/square-nodejs-sdk");
+const { Client, Environment } = require("@square/square");
 
 const app = express();
 app.use(cors());
