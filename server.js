755933e30215015480b077a94bd2229dfd5e2acd
chuchumtech <shlomo@chuchumwebdesign.com>
2025-04-16 20:12:46 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,9 +8,20 @@ require("dotenv").config();
 const app = express();
 
 // ✅ CORS setup to allow your live frontend
-app.use(cors({
-  origin: ["https://moesjerky.shop", "http://localhost:3000"]
-}));
+app.use((req, res, next) => {
+  const allowedOrigins = ["https://moesjerky.shop", "http://localhost:3000"];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 app.use(express.json());
 
